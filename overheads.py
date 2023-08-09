a8a02c149b78848c76bf091533cd4e5b6307846e
Dominique-tan <139714226+Dominique-tan@users.noreply.github.com>
2023-08-10 01:23:11 +0800

    Update overheads.py

--- overheads.py
+++ overheads.py
@@ -1,37 +1,40 @@
+#Import Path Function from pathlib
 from pathlib import Path
+#Import CSV module
 import csv
 
 # create function
 def overheads_function():
     """
-    - processes data from overheads.csv file to find highest overhead where results are written in Summary_report file
-    - no parameters required
+    -This function processes data from 'overheads.csv' file to find highestthe overhead
+    -No parameters is required
     """
 
-    # using the path module, set the file paths for the input and output files
+    #setup filepath for reading the "Profit_and_Loss.csv" file 
     overheads_fp_read= Path.cwd()/"csv_folder"/"overheads.csv"
+    #setup filepath for writing results to a text file, "Summary_report.txt)
     fp_write= Path.cwd()/"Summary_report.txt"
     
-     # create empty file to store summary report
+    #Creates the output text file at the specified path and filename if it doesn't already exist
     fp_write.touch()
 
-    # create empty list to store data from csv file
+    # create empty list to store overheads data
     overheads_list=[]
 
-    # open csv file to read data 
+    # open the "overheads.csv" file to read data 
     with overheads_fp_read.open(mode="r", encoding="UTF-8", newline="") as file:
         # create csv reader object to read file content as rows
         reader=csv.reader(file)
-        #skip the header in reader
+        #skip the header roe of the CSV file
         next(reader)
 
-        # use for loop to run each row of the file
+        # use for loop to iterate through each row in the csv fie
         for row in reader:
-            #extract values from first column of the row
+            #Extraxt the category from the CSV file
             category= row[0]
-            # convert values from second column of file to float
+            #Extract the Overheads percentage from the CSV file and convert them to float
             overhead=float(row[1])
-            # add category and overhead as a list to the main list
+            # add category and overhead to the overheads_list
             overheads_list.append([category,overhead])
 
     # create empty list to store values for highest overhead
@@ -39,13 +42,13 @@ def overheads_function():
     # set initial value of the percentage of overhead to be 0
     highest_overhead_percentage=0.00 
 
-    # using for loop, run each item in the list
+    # Iterate through the list of overheads to identify the category with the highest overhead percentage
     for category, overhead in overheads_list:
-        # compare overhead value to current overhead percentage value and determine if overhead value is higher than the current
+        # compare overhead value to current overhead percentage value and determine if the overhead value is higher than the current
         if overhead>highest_overhead_percentage:
-            # update highest value is overhead is higher
+            # update highest value if the current overhead is greater
             highest_overhead_percentage= overhead
-            # store category with the new highest overhead value
+            # store the category with the new highest overhead value
             highest_overhead_category= category 
 
     # create result string showing the category with the highest overhead percentage
