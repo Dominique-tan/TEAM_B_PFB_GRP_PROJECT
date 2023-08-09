cbd234ead46875c34b733aaece2a364e147632e0
Dominique-tan <139714226+Dominique-tan@users.noreply.github.com>
2023-08-10 00:59:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# Import the required modules from the Python standard library and your custom modules
-from pathlib import Path
 
-# Import cash_on_hand function 
-# Import overheads function 
-# Import profit_loss function 
+
+# Import cash_on_hand function, Import overheads function, Import profit_loss function 
 import cash_on_hand,overheads,profit_loss
 
-
-# Define the main function that will be executed
 def main():
-    # Call the function from the 'overheads' module to calculate and calculate highest overhead cost
+    """
+    This function calls functions from the 'cash_on_hand.py', 'overheads.py' and 'profit_loss.py' modules,
+    to calculate cash deficits, highest overheads and profit deficits
+    No parameters is required
+    """
+       
+    # Call the function from the 'overheads' module to calculate and identify the highest overhead cost
     overheads.overheads_function()
 
-    # Call the function from the 'cash_on_hand' module to calculate and calculate cash on hand deficit
+    # Call the function from the 'cash_on_hand' module to calculate and identify cash deficits
     cash_on_hand.cash_on_hand_function()
 
-    # Call the function from the 'profit_loss' module to calculate the profit deficit 
+    # Call the function from the 'profit_loss' module to calculate and identify the net profit deficit 
     profit_loss.net_profit()
 
 # Call the main function to start the program
